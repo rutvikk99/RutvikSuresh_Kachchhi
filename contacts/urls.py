42e44a8b1fe7686009df965d667b63a0f60f8e46
rutvikk99 <rutvikk99@gmail.com>
2023-12-07 17:15:58 -0800

    delete page

--- contacts/urls.py
+++ contacts/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('new/', views.new_contact, name='new_contact'),
     path('contact/<int:contact_id>/', views.contact_details, name='contact_details'),
+    path('contact/<int:contact_id>/edit/', views.edit_contact, name='edit_contact'),
+    path('contact/<int:contact_id>/delete/', views.delete_contact, name='delete_contact'),
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -21,3 +21,25 @@ def new_contact(request):
 def contact_details(request, contact_id):
     contact = get_object_or_404(Contact, pk=contact_id)
     return render(request, 'contacts/contact_details.html', {'contact': contact})
+
+def edit_contact(request, contact_id):
+    contact = get_object_or_404(Contact, pk=contact_id)
+
+    if request.method == 'POST':
+        form = ContactForm(request.POST, instance=contact)
+        if form.is_valid():
+            form.save()
+            return redirect('contact_details', contact_id=contact.id)
+    else:
+        form = ContactForm(instance=contact)
+
+    return render(request, 'contacts/edit_contact.html', {'form': form, 'contact': contact})
+
+def delete_contact(request, contact_id):
+    contact = get_object_or_404(Contact, pk=contact_id)
+
+    if request.method == 'POST':
+        contact.delete()
+        return redirect('home')
+
+    return render(request, 'contacts/delete_contact.html', {'contact': contact})
