5bee8e224519edf9dd4dcd054d944e5fd0f72783
rutvikk99 <rutvikk99@gmail.com>
2023-12-07 17:13:13 -0800

    Details Page

--- contacts/urls.py
+++ contacts/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('new/', views.new_contact, name='new_contact'),
+    path('contact/<int:contact_id>/', views.contact_details, name='contact_details'),
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -7,3 +7,17 @@ def home(request):
     contacts = Contact.objects.all()
     return render(request, 'contacts/home.html', {'contacts': contacts})
 
+def new_contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = ContactForm()
+    
+    return render(request, 'contacts/new_contact.html', {'form': form})
+
+def contact_details(request, contact_id):
+    contact = get_object_or_404(Contact, pk=contact_id)
+    return render(request, 'contacts/contact_details.html', {'contact': contact})
